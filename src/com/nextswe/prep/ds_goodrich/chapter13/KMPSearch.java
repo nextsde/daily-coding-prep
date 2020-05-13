f31879c797ad3481d7a6ca87663b3f97c67d0293
nextswe <>
2020-05-13 14:40:17 +0600

    KMP from book

--- src/com/nextswe/prep/ds_goodrich/chapter13/KMPSearch.java
+++ src/com/nextswe/prep/ds_goodrich/chapter13/KMPSearch.java
@@ -45,6 +45,48 @@ public class KMPSearch {
     }
 
 
+    /*
+    Returns the lowest index at which substring pattern begins in text (or else −1)
+     */
+    public static int findKMP(String text, String pattern){
+        int n = text.length();
+        int m = pattern.length();
+        if(m==0) return 0; // trivial search for empty string
+        int[] lps = computeFailKMP(pattern); // computed by private utility
+        int j = 0; // index into text
+        int k = 0; // index into pattern
+        while(j<n){
+            if(text.charAt(j)==pattern.charAt(k)){ // pattern[0..k] matched thus far
+                if(k==m-1) return j-m+1; // match is complete
+                j++; // otherwise, try to extend match
+                k++;
+            }else if(k>0){
+                k = lps[k-1]; // reuse suffix of P[0..k-1]
+            }else{
+                j++;
+            }
+        }
+        return -1; // reached end without match
+    }
+
+    private static int[] computeFailKMP(String pattern){
+        int m = pattern.length();
+        int[] fail = new int[m];
+        int j = 1;
+        int k = 0;
+        while(j<m){
+            if(pattern.charAt(j) == pattern.charAt(k)){
+                fail[j] = k+1;
+                j++;
+                k++;
+            }else if(k>0){
+                k = fail[k-1];
+            }else{
+                j++;
+            }
+        }
+        return fail;
+    }
 
     public static void main(String[] args){
         System.out.println(kmpSearch(
